53414625b7cde2ac9e83aa2de3117e90bef1adb6
Jonathan Juayong <jonathan.juayong@gmail.com>
2023-05-28 01:59:44 +1000

    create inline component

---
+++ components/layout/Inline/index.tsx
@@ -0,0 +1,67 @@
+import {Children, cloneElement, CSSProperties, ElementType, ReactElement, ReactNode} from "react";
+import Polymorphic, {PolymorphicComponent} from "@/components/Polymorphic";
+import styles from "./styles.module.scss"
+import theme, {Spacing} from "@/utils/theme";
+
+type PositionOptions = "center"
+  | "end"
+  | "start"
+  | "stretch"
+  | "between"
+  | "around"
+  | "evenly"
+
+type PositionMap = { [key in PositionOptions]: string };
+
+interface InlineProps extends PolymorphicComponent<ElementType> {
+  children: ReactNode
+  gutter?: Spacing
+  justify?: PositionOptions
+  align?: PositionOptions
+}
+
+export default function Inline(
+  {
+    children,
+    as = "div",
+    gutter = "0",
+    justify = "between",
+    align = "start"
+  }: InlineProps) {
+
+  const positionMap: PositionMap = {
+    "start": "flex-start",
+    "center": "center",
+    "end": "flex-end",
+    "stretch": "initial",
+    "between": "space-between",
+    "around": "space-around",
+    "evenly": "space-evenly",
+  }
+
+  const style = {
+    "--gutter": theme.spacing[gutter],
+    "--justify": positionMap[justify],
+    "--align": positionMap[align],
+  } as CSSProperties
+
+  return (
+    <Polymorphic
+      style={style}
+      as={as}
+      className={styles.inline}
+    >
+      {children}
+    </Polymorphic>
+  )
+}
+
+function Stretch({children}: {children: ReactNode}) {
+  const child = Children.only(children) as ReactElement
+  const stretched = cloneElement(child, {
+    className: styles.stretch
+  })
+  return <>{stretched}</>
+}
+
+Inline.Stretch = Stretch
