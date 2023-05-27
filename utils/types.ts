385a04739af040a3b6f34f6dea73af656d54d494
Jonathan Juayong <jonathan.juayong@gmail.com>
2023-05-27 13:39:02 +1000

    create types

---
+++ utils/types.ts
@@ -0,0 +1,31 @@
+export interface IngredientItem {
+  name: string
+  unit: string
+  qty: number
+  note: string
+}
+
+export interface IngredientGroup {
+  name: string
+  items: IngredientItem[]
+}
+
+export interface Instruction {
+  image: string
+  description: string
+}
+
+export interface Recipe {
+  header: string
+  subheader: string
+  description: string
+  ingredients: IngredientGroup[]
+  instructions: Instruction[]
+  author: string
+  yield: number
+  prepTime: number
+  cookTime: number
+  images: string[]
+  equipment: string[]
+  allergens?: string[]
+}
